a17a595731535b1f8b04f8a7fb5c45ae1dc95713
Marvin Knoll <knollmarvin6@gmail.com>
2021-11-16 19:33:11 +0100

    fix: CORS configuration

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import helmet from "helmet";
 import dotenv from "dotenv";
-import cors from "cors";
 import indexRouter from "./api/routes";
 
 dotenv.config();
 
-let allowedOrigins = ["https://www.tulip.florist"];
+let allowedOrigins = ["https://www.tulip.florist", "https://tulip.florist"];
 if (process.env.NODE_ENV != "production") {
   allowedOrigins = [...allowedOrigins, "http://localhost:3000"];
 }
-const corsOptions = {
-  origin: allowedOrigins,
-  exposedHeaders: "Authorization",
-};
 
 const PORT = process.env.PORT || 8080;
 const app: Express = express();
@@ -22,6 +17,18 @@ app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/v1/", cors(corsOptions), indexRouter);
+app.use(function (req, res, next) {
+  const origin = req.headers.origin as string;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use("/v1/", indexRouter);
 
 app.listen(PORT, () => console.log(`Running on ${PORT}`));
