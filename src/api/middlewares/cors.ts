b2a38249c5a0e56c54b35265457394df74e929e7
Marvin Knoll <knollmarvin6@gmail.com>
2021-11-19 17:30:06 +0100

    fix: preflight response status

---
+++ src/api/middlewares/cors.ts
@@ -0,0 +1,31 @@
+import { Response, NextFunction, Request } from "express";
+
+let allowedOrigins = ["https://www.tulip.florist", "https://tulip.florist"];
+if (process.env.NODE_ENV != "production") {
+  allowedOrigins = [...allowedOrigins, "http://localhost:3000"];
+}
+
+export const corsMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const origin = req.headers.origin as string;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "POST,GET,OPTIONS,PUT,DELETE");
+  res.header("Access-Control-Expose-Headers", "Authorization"); // Required to have acces to Auth header in frontend
+
+  if (req.method === "OPTIONS") {
+    // Preflight requests
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,9 @@ import express, { Express, Request, Response } from "express";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import indexRouter from "./api/routes";
+import { corsMiddleware } from "./api/middlewares/cors";
 
 dotenv.config();
-
-let allowedOrigins = ["https://www.tulip.florist", "https://tulip.florist"];
-if (process.env.NODE_ENV != "production") {
-  allowedOrigins = [...allowedOrigins, "http://localhost:3000"];
-}
 
 const PORT = process.env.PORT || 8080;
 const app: Express = express();
@@ -17,18 +13,7 @@ app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  const origin = req.headers.origin as string;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
+app.use(corsMiddleware);
 app.use("/v1/", indexRouter);
 
 app.listen(PORT, () => console.log(`Running on ${PORT}`));
