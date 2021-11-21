ae7e7f7c4acf2b12327af07ea52503caccc7b357
Marvin Knoll <knollmarvin6@gmail.com>
2021-11-21 11:47:02 +0100

    fix: add authentication middleware to /me

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { emailSignin, emailSignup, me } from "../controllers/auth";
+import { authenticate } from "../middlewares/authJWT";
 const authRouter = express.Router();
 
 authRouter.post("/emailSignup", emailSignup);
 authRouter.post("/emailSignin", emailSignin);
-authRouter.get("/me", me);
+authRouter.get("/me", authenticate, me);
 
 export default authRouter;
