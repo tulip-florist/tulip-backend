8d5a276fbb4c8eebbab250e16b78aad1ded92108
Marvin Knoll <knollmarvin6@gmail.com>
2021-12-03 11:26:14 +0100

    feat: winston + morgan + rotation logging

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
+require("dotenv").config();
 import express, { Express, Request, Response } from "express";
-import morgan from "morgan";
+import logger from "./util/logger";
+import httpLogger from "./util/httpLogger";
 import helmet from "helmet";
-import dotenv from "dotenv";
 import indexRouter from "./api/routes";
 import { corsMiddleware } from "./api/middlewares/cors";
-
-dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 const app: Express = express();
@@ -13,9 +12,9 @@ const app: Express = express();
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(httpLogger);
 
 app.use(corsMiddleware);
 app.use("/v1/", indexRouter);
 
-app.listen(PORT, () => console.log(`Running on ${PORT}`));
+app.listen(PORT, () => logger.info(`Running on ${PORT}`));
---
+++ src/util/logger.ts
@@ -0,0 +1,40 @@
+import winston from "winston";
+import DailyRotateFile from "winston-daily-rotate-file";
+
+const options = {
+  file: {
+    filename: "./logs/app-%DATE%.log",
+    datePattern: "YYYY-MM-DD-HH",
+    frequency: "24h",
+    zippedArchive: true,
+    maxSize: "20m",
+    maxFiles: "14d",
+    level: process.env.LOG_LEVEL_FILE,
+  },
+  console: {
+    level: process.env.LOG_LEVEL_CONSOLE,
+  },
+  exception: {
+    filename: "./logs/exceptions.log",
+  },
+};
+
+const format = winston.format.combine(
+  winston.format.timestamp({
+    format: "YYYY-MM-DD HH:mm:ss",
+  }),
+  winston.format.simple()
+);
+
+const logger = winston.createLogger({
+  levels: winston.config.npm.levels,
+  format,
+  transports: [
+    new DailyRotateFile(options.file),
+    new winston.transports.Console(options.console),
+  ],
+  exceptionHandlers: [new winston.transports.File(options.exception)],
+  exitOnError: false,
+});
+
+export default logger;
