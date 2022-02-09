e6b1c38f7400417fa68b248c7926976a0527c0ee
Marvin Knoll <knollmarvin6@gmail.com>
2022-02-09 10:44:58 +0100

    ref: documentAPI Limiter before auth

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from "express";
 import { AuthRequest } from "../../types/types";
 import { authenticate } from "../middlewares/authJWT";
+import { documentApiLimiter } from "../middlewares/rateLimit";
 import authRouter from "./auth";
 import documentRouter from "./document";
 
 const indexRouter = express.Router();
 
 indexRouter.use("/auth", authRouter);
-indexRouter.use("/documents", authenticate, documentRouter);
+indexRouter.use("/documents", documentApiLimiter, authenticate, documentRouter);
 indexRouter.get("/health", (req, res) => {
   res.sendStatus(200);
 });
